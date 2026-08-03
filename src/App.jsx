f1c1db3733b451dd0fe79e9b8355039f6faa7ae9
Oleg Chaplashkin <olegchaplashkin@gmail.com>
2026-08-03 23:45:59 +0400

    feat: add Quill lib and basic editor
    Quill [1] was chosen because it was easy to use for my needs. I
    added a simple editor following the instructions.
    
    [1]: https://github.com/slab/quill/

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { useRef, useState } from 'react';
+import Quill from 'quill/core';
+import Editor from '/src/components/editor/Editor.jsx';
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Delta = Quill.import('delta');
+
+const App = () => {
+  const [range, setRange] = useState();
+  const [lastChange, setLastChange] = useState();
+  const [readOnly, setReadOnly] = useState(false);
+
+  // Use a ref to access the quill instance directly
+  const quillRef = useRef();
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+    <div>
+      <Editor
+        ref={quillRef}
+        readOnly={readOnly}
+        defaultValue={new Delta()
+          .insert('Hello')
+          .insert('\n', { header: 1 })
+          .insert('Some ')
+          .insert('initial', { bold: true })
+          .insert(' ')
+          .insert('content', { underline: true })
+          .insert('\n')}
+        onSelectionChange={setRange}
+        onTextChange={setLastChange}
+      />
+      <div className="controls">
+        <label>
+          Read Only:{' '}
+          <input
+            type="checkbox"
+            value={readOnly}
+            onChange={(e) => setReadOnly(e.target.checked)}
+          />
+        </label>
         <button
+          className="controls-right"
           type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => {
+            alert(quillRef.current?.getLength());
+          }}
         >
-          Count is {count}
+          Get Content Length
         </button>
-      </section>
+      </div>
+      <div className="state">
+        <div className="state-title">Current Range:</div>
+        {range ? JSON.stringify(range) : 'Empty'}
+      </div>
+      <div className="state">
+        <div className="state-title">Last Change:</div>
+        {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
+      </div>
+    </div>
+  );
+};
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
-
-export default App
+export default App;
